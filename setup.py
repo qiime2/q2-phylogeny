51e30247ac60b1f1201b8e6fe4b656e2bdd16779
Greg Caporaso <gregcaporaso@gmail.com>
2016-08-04 10:37:32 -0700

    ENH: initial package structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+setup(
+    name="q2-phylogeny",
+    # TODO stop duplicating version string
+    version="0.0.0-dev",
+    packages=find_packages(),
+    install_requires=['scikit-bio', 'qiime >= 2.0.0', 'q2-types'],
+    #package_data={'q2_phylogeny': ['markdown/*md']},
+    author="Greg Caporaso",
+    author_email="gregcaporaso@gmail.com",
+    description="Create and work with Phylogenetic trees in QIIME 2.",
+    license="BSD",
+    url="http://www.qiime.org",
+    entry_points={
+        'qiime.plugins': ['q2-phylogeny=q2_phylogeny.plugin_setup:plugin']
+    }
+)
