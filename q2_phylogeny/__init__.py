b9e6b7ac473b8329e6091c4f647061dc5cb947c9
Matthew Dillon <matthewrdillon@gmail.com>
2016-12-23 08:43:25 -0700

    ENH: new date-based versioning; `qiime` -> `qiime2` package rename
    New date-based versioning scheme is:
    
    - YYYY.M.patch.dev0 for development version (e.g. 2017.2.0.dev0, 2017.2.1.dev0, 2018.10.0.dev0)
    - YYYY.M.patch for release version (e.g. 2017.2.0, 2017.2.1, 2018.10.0)
    
    Plugins/interfaces are recommended to specify `qiime2 == YYYY.M.*` in setup.py's `install_requires` to obtain patch releases for a given train release.
    
    The new versioning scheme is PEP 440 compliant.

--- q2_phylogeny/__init__.py
+++ q2_phylogeny/__init__.py
@@ -1,15 +1,18 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME development team.
+# Copyright (c) 2016-2017, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import pkg_resources
+
 from ._util import midpoint_root
 from ._fasttree import fasttree
 from ._filter import filter_table
 
-__version__ = '0.0.7.dev0'
+
+__version__ = pkg_resources.get_distribution('q2-phylogeny').version
 
 __all__ = ["midpoint_root", "fasttree", "filter_table"]
