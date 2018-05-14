dbfe8dbf80fb0c9ce6e5d8664fa0e3b6acec3f30
mikerobeson <soilbdelloid@gmail.com>
2018-05-14 15:27:38 -0500

    added raxml model choice tests

--- q2_phylogeny/tests/test_raxml.py
+++ q2_phylogeny/tests/test_raxml.py
@@ -24,7 +24,11 @@ class RaxmlTests(TestPluginBase):
 
     package = 'q2_phylogeny.tests'
 
+
     def test_raxml(self):
+        """Test that output tree is made.
+        Reads tree output and compares tip labels to expected labels.
+        """
         input_fp = self.get_data_path('aligned-dna-sequences-3.fasta')
         input_sequences = AlignedDNAFASTAFormat(input_fp, mode='r')
         with redirected_stdio(stderr=os.devnull):
@@ -38,7 +42,12 @@ class RaxmlTests(TestPluginBase):
         'GCA000454205', 'GCA000473545','GCA000196255','GCA002142615',
         'GCA000686145','GCA001950115','GCA001971985','GCA900007555']))
 
+
     def test_raxml_underscore_ids(self):
+        """Test that output tree is made with underscores in tip IDs.
+        Some programs and python wrappers may strip underscores.
+        Reads tree output and compares tip labels to expected labels.
+        """
         input_fp = self.get_data_path('aligned-dna-sequences-4.fasta')
         input_sequences = AlignedDNAFASTAFormat(input_fp, mode='r')
         with redirected_stdio(stderr=os.devnull):
@@ -53,7 +62,9 @@ class RaxmlTests(TestPluginBase):
         'GCA_000196255_1', 'GCA_002142615_1', 'GCA_000686145_1',
         'GCA_001950115_1','GCA_001971985_1', 'GCA_900007555_1']))
 
+
     def test_raxml_n_threads(self):
+        """Test that an output tree is made when invoking threads."""
         input_fp = self.get_data_path('aligned-dna-sequences-3.fasta')
         input_sequences = AlignedDNAFASTAFormat(input_fp, mode='r')
         with redirected_stdio(stderr=os.devnull):
@@ -67,28 +78,61 @@ class RaxmlTests(TestPluginBase):
             'GCA000473545','GCA000196255','GCA002142615','GCA000686145',
             'GCA001950115','GCA001971985','GCA900007555']))
 
-    # To do:
-    # Assert that the collection of branchlengths are as expected
-    # when switching the default model and/or test tree topology.
-    #
-    # Test that an invalid model choice can be captured? I think
-    # this may already be handled by setting up 'Choices' for this.
-    #
-    # def test_raxml_model_choice(self):
-    #     input_fp = self.get_data_path('aligned-dna-sequences-3.fasta')
-    #     input_sequences = AlignedDNAFASTAFormat(input_fp, mode='r')
-    #     with redirected_stdio(stderr=os.devnull):
-    #         obs = raxml(input_sequences, substitution_model='GTRCAT')
-    #     obs_tree = skbio.TreeNode.read(str(obs), convert_underscores=False)
-    #     # run raxml manually and pasted resulting newick tree below
-    #     # then compare them. Alternatively, capture a set of branchlengths.
-    #     exp_tree = ''
-    #
-    # def test_raxml_invalid_model_choice(self):
-    #     input_fp = self.get_data_path('aligned-dna-sequences-3.fasta')
-    #     input_sequences = AlignedDNAFASTAFormat(input_fp, mode='r')
-    #     with redirected_stdio(stderr=os.devnull):
-    #         obs = raxml(input_sequences, substitution_model='JC69')
+
+    def test_raxml_default_seed(self):
+        """Test tip-to-tip dists are identical to manually run RAxML output.
+        This test is comparing an ordered series of tip-to-tip distances
+        to a tree output from a manual run of the default command:
+            raxmlHPC -m GTRGAMMA -p 1723 -s aligned-dna-sequences-3.fasta
+                -w tmp_dir -n q2
+        """
+        input_fp = self.get_data_path('aligned-dna-sequences-3.fasta')
+        input_sequences = AlignedDNAFASTAFormat(input_fp, mode='r')
+
+        with redirected_stdio(stderr=os.devnull):
+            obs = raxml(input_sequences)
+            obs_tree = skbio.TreeNode.read(str(obs), convert_underscores=False)
+            obs_series = set(obs_tree.tip_tip_distances().to_series())
+
+        exp_tree = skbio.TreeNode.read(self.get_data_path('test.tre'))
+        exp_series = set(exp_tree.tip_tip_distances().to_series())
+
+        self.assertEqual(obs_series, exp_series)
+
+
+    def test_raxml_model_choice(self):
+        """Tip to tip dists should NOT be identical under different models.
+        Default is GTRGAMMA, we'll compare ouput to GRTGAMMAI & GTRCAT.
+
+        This test is comparing an ordered series of tip-to-tip distances.
+        """
+        input_fp = self.get_data_path('aligned-dna-sequences-3.fasta')
+        input_sequences = AlignedDNAFASTAFormat(input_fp, mode='r')
+
+        # default GTRGAMMA
+        with redirected_stdio(stderr=os.devnull):
+            gtrg = raxml(input_sequences)
+            gtrg_tree = skbio.TreeNode.read(str(gtrg), convert_underscores=False)
+            gtrg_td = set(gtrg_tree.tip_tip_distances().to_series())
+
+        # set GTRGAMMAI
+        with redirected_stdio(stderr=os.devnull):
+            gtrgi = raxml(input_sequences, substitution_model='GTRGAMMAI')
+            gtrgi_tree = skbio.TreeNode.read(str(gtrgi), convert_underscores=False)
+            gtrgi_td = set(gtrgi_tree.tip_tip_distances().to_series())
+
+        # set GTRCAT
+        with redirected_stdio(stderr=os.devnull):
+            gtrcat = raxml(input_sequences, substitution_model='GTRCAT')
+            gtrcat_tree = skbio.TreeNode.read(str(gtrcat), convert_underscores=False)
+            gtrcat_td = set(gtrcat_tree.tip_tip_distances().to_series())
+            # compare trees, each should have different branch lengths
+
+        # test pairs are not equivalent
+        self.assertNotEqual(gtrg_td, gtrgi_td)
+        self.assertNotEqual(gtrg_td, gtrcat_td)
+        self.assertNotEqual(gtrgi_td, gtrcat_td)
+
 
 
 
