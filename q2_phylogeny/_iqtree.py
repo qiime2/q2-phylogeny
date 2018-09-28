5116ffcdc976ad63f395e06508003e52766bf11a
mikerobeson <soilbdelloid@gmail.com>
2018-09-28 14:01:59 -0500

    started adding access to new iqtree features

--- q2_phylogeny/_iqtree.py
+++ q2_phylogeny/_iqtree.py
@@ -16,6 +16,7 @@ from q2_phylogeny._raxml import run_command
 _iqtree_defaults = {
     'seed': None,
     'n_cores': 1,
+    'n_runs': 1,
     'substitution_model': 'MFP',
     'run_prefix': 'q2iqtree',
     'dtype': 'DNA',
@@ -26,7 +27,12 @@ _iqtree_defaults = {
     'stop_iter': None,
     'perturb_nni_strength': None,
     'spr_radius': None,
+    'fast': False,
+    'alrt': None,
+    'abayes': False,
+    'lbp': None,
     'allnni': False,
+    'bnni': False,
     'safe': False,
     'bootstrap_replicates': 1000,
     'n_max_ufboot_iter': None,
@@ -40,6 +46,7 @@ def _build_iqtree_command(
         alignment,
         seed: int=_iqtree_defaults['seed'],
         n_cores: int=_iqtree_defaults['n_cores'],
+        n_runs: int=_iqtree_defaults['n_runs'],
         substitution_model: str=_iqtree_defaults['substitution_model'],
         run_prefix: str=_iqtree_defaults['run_prefix'],
         dtype: str=_iqtree_defaults['dtype'],
@@ -51,11 +58,16 @@ def _build_iqtree_command(
         perturb_nni_strength: float=_iqtree_defaults['perturb_nni_strength'],
         spr_radius: int=_iqtree_defaults['spr_radius'],
         allnni: bool=_iqtree_defaults['allnni'],
+        fast: bool=_iqtree_defaults['fast'],
+        alrt: int=_iqtree_defaults['alrt'],
+        abayes: bool=_iqtree_defaults['abayes'],
+        lbp: int=_iqtree_defaults['lbp'],
         safe: bool=_iqtree_defaults['safe']):
 
     cmd = ['iqtree']
 
     cmd += ['-st', str(dtype),
+            '--runs', str(n_runs),
             '-s', str(alignment),
             '-m', str(substitution_model),
             '-pre', str(run_prefix)]
@@ -70,6 +82,18 @@ def _build_iqtree_command(
 
     if safe:
         cmd += ['-safe']
+
+    if fast:
+        cmd += ['-fast']
+
+    if alrt:
+        cmd += ['-alrt', '%i' % alrt]
+
+    if abayes:
+        cmd += ['-abayes']
+
+    if lbp:
+        cmd += ['-lbp', '%i' % lbp]
 
     if allnni:
         cmd += ['-allnni']
@@ -102,6 +126,7 @@ def iqtree(
     alignment: AlignedDNAFASTAFormat,
     seed: int=_iqtree_defaults['seed'],
     n_cores: int=_iqtree_defaults['n_cores'],
+    n_runs: int=_iqtree_defaults['n_runs'],
     substitution_model: str=_iqtree_defaults['substitution_model'],
     n_init_pars_trees: int=_iqtree_defaults['n_init_pars_trees'],
     n_top_init_trees: int=_iqtree_defaults['n_top_init_trees'],
@@ -111,6 +136,10 @@ def iqtree(
     perturb_nni_strength: float=_iqtree_defaults['perturb_nni_strength'],
     spr_radius: int=_iqtree_defaults['spr_radius'],
     allnni: bool=_iqtree_defaults['allnni'],
+    fast: bool=_iqtree_defaults['fast'],
+    alrt: int=_iqtree_defaults['alrt'],
+    abayes: bool=_iqtree_defaults['abayes'],
+    lbp: int=_iqtree_defaults['lbp'],
     safe: bool=_iqtree_defaults['safe'],
             ) -> NewickFormat:
     result = NewickFormat()
@@ -120,6 +149,7 @@ def iqtree(
         cmd = _build_iqtree_command(alignment,
                                     seed=seed,
                                     n_cores=n_cores,
+                                    n_runs=n_runs,
                                     substitution_model=substitution_model,
                                     run_prefix=run_prefix,
                                     n_init_pars_trees=n_init_pars_trees,
@@ -130,6 +160,10 @@ def iqtree(
                                     perturb_nni_strength=perturb_nni_strength,
                                     spr_radius=spr_radius,
                                     allnni=allnni,
+                                    fast=fast,
+                                    alrt=alrt,
+                                    abayes=abayes,
+                                    lbp=lbp,
                                     safe=safe)
         run_command(cmd)
 
@@ -143,6 +177,7 @@ def _build_iqtree_ufbs_command(
         alignment,
         seed: int=_iqtree_defaults['seed'],
         n_cores: int=_iqtree_defaults['n_cores'],
+        n_runs:  int=_iqtree_defaults['n_runs'],
         substitution_model: str=_iqtree_defaults['substitution_model'],
         bootstrap_replicates: int=_iqtree_defaults['bootstrap_replicates'],
         run_prefix: str=_iqtree_defaults['run_prefix'],
@@ -158,6 +193,10 @@ def _build_iqtree_ufbs_command(
         min_cor_ufboot: float=_iqtree_defaults['min_cor_ufboot'],
         ep_break_ufboot: float=_iqtree_defaults['ep_break_ufboot'],
         allnni: bool=_iqtree_defaults['allnni'],
+        alrt: int=_iqtree_defaults['alrt'],
+        abayes: bool=_iqtree_defaults['abayes'],
+        lbp: int=_iqtree_defaults['lbp'],
+        bnni: bool=_iqtree_defaults['bnni'],
         safe: bool=_iqtree_defaults['safe']):
     # This is a separate command becuase there are several
     # bootstrap specific options.
@@ -165,6 +204,7 @@ def _build_iqtree_ufbs_command(
     cmd = ['iqtree', '-bb', '%i' % bootstrap_replicates]
 
     cmd += ['-st', str(dtype),
+            '--runs', str(n_runs),
             '-s', str(alignment),
             '-m', str(substitution_model),
             '-pre', str(run_prefix)]
@@ -182,6 +222,18 @@ def _build_iqtree_ufbs_command(
 
     if allnni:
         cmd += ['-allnni']
+
+    if alrt:
+        cmd += ['-alrt', '%i' % alrt]
+
+    if abayes:
+        cmd += ['-abayes']
+
+    if lbp:
+        cmd += ['-lbp', '%i' % lbp]
+
+    if bnni:
+        cmd+= ['-bnni']
 
     if n_init_pars_trees:
         cmd += ['-ninit', str(n_init_pars_trees)]
@@ -220,6 +272,7 @@ def iqtree_ultrafast_bootstrap(
     alignment: AlignedDNAFASTAFormat,
     seed: int=_iqtree_defaults['seed'],
     n_cores: int=_iqtree_defaults['n_cores'],
+    n_runs: int=_iqtree_defaults['n_runs'],
     substitution_model: str=_iqtree_defaults['substitution_model'],
     bootstrap_replicates: int=_iqtree_defaults['bootstrap_replicates'],
     n_init_pars_trees: int=_iqtree_defaults['n_init_pars_trees'],
@@ -233,10 +286,14 @@ def iqtree_ultrafast_bootstrap(
     min_cor_ufboot: float=_iqtree_defaults['min_cor_ufboot'],
     ep_break_ufboot: float=_iqtree_defaults['ep_break_ufboot'],
     allnni: bool=_iqtree_defaults['allnni'],
+    alrt: int=_iqtree_defaults['alrt'],
+    abayes: bool=_iqtree_defaults['abayes'],
+    lbp: int=_iqtree_defaults['lbp'],
+    bnni: bool=_iqtree_defaults['bnni'],
     safe: bool=_iqtree_defaults['safe']
                                 ) -> NewickFormat:
-    # NOTE: the IQ-TREE command `-n` (called as `n_iter` in the `iqtree`
-    # method) is not compatable with ultrafast_bootstrap `-bb`
+    # NOTE: the IQ-TREE commands `-n` (called as `n_iter` in the `iqtree`
+    # method)and `-fast` is not compatable with ultrafast_bootstrap `-bb`.  
     result = NewickFormat()
 
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -245,6 +302,7 @@ def iqtree_ultrafast_bootstrap(
                     alignment,
                     seed=seed,
                     n_cores=n_cores,
+                    n_runs=n_runs,
                     substitution_model=substitution_model,
                     bootstrap_replicates=bootstrap_replicates,
                     run_prefix=run_prefix,
@@ -259,6 +317,10 @@ def iqtree_ultrafast_bootstrap(
                     min_cor_ufboot=min_cor_ufboot,
                     ep_break_ufboot=ep_break_ufboot,
                     allnni=allnni,
+                    alrt=alrt,
+                    abayes=abayes,
+                    lbp=lbp,
+                    bnni=bnni,
                     safe=safe)
         run_command(cmd)
         tree_tmp_fp = os.path.join(temp_dir, '%s.treefile' % run_prefix)
