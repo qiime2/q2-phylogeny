30ad8602d2b33db2a76276507d9eeadd5d1f5a2f
Justine <j.debelius@gmail.com>
2021-08-11 10:32:07 -0600

    Refactor to filter with sequences, tables, or metadata
    (As suggested by @nbokulich and @mikerobeson)

--- q2_phylogeny/_filter.py
+++ q2_phylogeny/_filter.py
@@ -7,6 +7,9 @@
 # ----------------------------------------------------------------------------
 
 import biom
+import numpy as np
+import pandas as pd
+import qiime2
 import skbio
 
 
@@ -21,17 +24,43 @@ def filter_table(table: biom.Table, tree: skbio.TreeNode) -> biom.Table:
     return table
 
 
-def filter_tree(table: biom.Table, tree: skbio.TreeNode) -> skbio.TreeNode:
+def filter_tree(tree: skbio.TreeNode, 
+                table: biom.Table=None, 
+                sequences: pd.Series=None, 
+                metadata: qiime2.Metadata=None,
+                where: str=None,
+                ) -> skbio.TreeNode:
     """
-    Prunes a phylogenetic tree to match the input feature table
+    Prunes a phylogenetic tree to match the input ids
     """
+    # Checks the input metadata
+    if ((table is None) & (sequences is None) & (metadata is None)):
+        raise ValueError('A feature table, sequences or metadata must be '
+                         'provided for filtering.')
+    filter_refs = [table, sequences, metadata]
+    if np.sum([(ref is not None) for ref in filter_refs]).sum() > 1:
+        raise ValueError('Filtering can only be performed using one reference'
+                         ' file. Please choose between filtering with a '
+                         'feature table, sequences, or metadata.')
+    if (where is not None) & (metadata is None):
+        raise ValueError("Metadata must be provided if 'where' is specified")
+
+    # Gets the list of IDs to keep
+    if table is not None:
+        ids_to_keep = table.ids(axis='observation')
+    if sequences is not None:
+        ids_to_keep = sequences.index 
+    if metadata is not None:
+        ids_to_keep = metadata.get_ids(where)
+
+    # Gets the list of tips
     tip_ids = set([t.name for t in tree.tips()])
-    feature_ids = set(table.ids(axis='observation'))
-    # ids_to_keep can only include ids that are in table
-    ids_to_keep = tip_ids & feature_ids
-    if len(ids_to_keep) == 0:
-        raise ValueError("There are no ids shared between the table and"
-                         " the tree.")
-    sub_tree = tree.shear(ids_to_keep).prune()
+
+    # Checks for an intersection between ids
+    if not set(tip_ids).issuperset(set(ids_to_keep)):
+        raise ValueError('The ids for filtering must be a subset of '
+                         'the tips in the tree.')
+    sub_tree = tree.shear(ids_to_keep)
+    sub_tree.prune()
 
     return sub_tree
--- q2_phylogeny/tests/test_filter.py
+++ q2_phylogeny/tests/test_filter.py
@@ -10,8 +10,11 @@ import unittest
 import io
 
 import numpy as np
+import pandas as pd
 from biom.table import Table
 import skbio
+
+from qiime2 import Metadata
 
 from q2_phylogeny import (filter_table, filter_tree)
 
@@ -50,35 +53,85 @@ class FilterTableTests(unittest.TestCase):
 
 
 class FilterTreeTests(unittest.TestCase):
+    def setUp(self):
+        rooted_nwk = io.StringIO("((A:0.1, B:0.2)C:0.3, D:0.4, E:0.5)root;")
+        self.tree = skbio.TreeNode.read(rooted_nwk)
+        self.metadata = Metadata(pd.DataFrame(
+                data=np.array([['Bacteria', '1'],
+                               ['Archea', '1']], dtype=object),
+                index=pd.Index(['A', 'D'], name='Feature ID'),
+                columns=['kingdom', 'keep'],
+            ))
+        self.table = Table(data=np.array([[0, 1, 2], [2, 2, 2]]),
+                           observation_ids=['A', 'D'],
+                           sample_ids=['S1', 'S2', 'S3']
+                           )
+        self.sequences = pd.Series(
+            data=[skbio.DNA('CAT', metadata={'id': 'A'}),
+                  skbio.DNA('GGG', metadata={'id': 'D'}),
+                  ],
+            index=pd.Index(['A', 'D'], name='sample-id')
+            )
+        self.filtered_tree = self.tree.copy().shear(['A', 'D'])
+        self.filtered_tree.prune()
 
-    def test_filter_tree_some_intersection(self):
-        rooted_nwk = io.StringIO("(O1:4.5,(d:4,(a:1,b:1):2):0.5);")
-        tree = skbio.TreeNode.read(rooted_nwk)
-        table = Table(np.array([[0, 1, 3], [1, 1, 2]]),
-                      ['O1', 'O2'],
-                      ['S1', 'S2', 'S3'])
-        actual = filter_tree(table, tree)
-        expected = tree.shear(['O1']).prune()
-        self.assertEqual(actual, expected)
+    def test_filter_tree_error_no_filter_art(self):
+        with self.assertRaises(ValueError) as err:
+            filter_tree(self.tree)
+        self.assertEqual(
+            str(err.exception),
+            ('A feature table, sequences or metadata must be provided for '
+                'filtering.')
+            )
 
-    def test_filter_tree_no_intersection(self):
-        rooted_nwk = io.StringIO("(c:4.5,(d:4,(a:1,b:1):2):0.5);")
-        tree = skbio.TreeNode.read(rooted_nwk)
-        table = Table(np.array([[0, 1, 3], [1, 1, 2]]),
-                      ['O1', 'O2'],
-                      ['S1', 'S2', 'S3'])
-        with self.assertRaises(ValueError):
-            filter_tree(table, tree)
+    def test_filter_tree_error_multiple_filter_arts(self):
+        with self.assertRaises(ValueError) as err:
+            filter_tree(self.tree,
+                        table=self.table,
+                        sequences=self.sequences,
+                        metadata=self.metadata)
+        self.assertEqual(
+            str(err.exception),
+            ('Filtering can only be performed using one reference file. '
+             'Please choose between filtering with a feature table, '
+             'sequences, or metadata.')
+        )
 
-    def test_filter_tree_perfect_intersection(self):
-        rooted_nwk = io.StringIO("(O1:4.5,(O2:4,(a:1,b:1):2):0.5);")
-        tree = skbio.TreeNode.read(rooted_nwk)
-        table = Table(np.array([[0, 1, 3], [1, 1, 2]]),
-                      ['O1', 'O2'],
-                      ['S1', 'S2', 'S3'])
-        actual = filter_tree(table, tree)
-        expected = tree.shear(['O1', 'O2']).prune()
-        self.assertEqual(actual, expected)
+    def test_filter_tree_error_where_no_metadata(self):
+        with self.assertRaises(ValueError) as err:
+            filter_tree(self.tree,
+                        table=self.table,
+                        where='[kingdom]="Archea"')
+        self.assertEqual(
+            str(err.exception),
+            ("Metadata must be provided if 'where' is specified")
+            )
+
+    def test_filter_tree_error_filter_superset(self):
+        metadata = Metadata(pd.DataFrame(
+            data=np.array([[1, 1, 0]]).T,
+            index=pd.Index(['A', 'D', 'F'], name='feature-id'),
+            columns=['keep']
+        ))
+        with self.assertRaises(ValueError) as err:
+            filter_tree(self.tree,
+                        metadata=metadata)
+        self.assertEqual(
+            str(err.exception),
+            'The ids for filtering must be a subset of the tips in the tree.'
+            )
+
+    def test_filter_tree_metadata(self):
+        test = filter_tree(self.tree, metadata=self.metadata)
+        self.assertEqual(str(test), str(self.filtered_tree))
+
+    def test_filter_tree_table(self):
+        test = filter_tree(self.tree, table=self.table)
+        self.assertEqual(str(test), str(self.filtered_tree))
+
+    def test_filter_tree_sequences(self):
+        test = filter_tree(self.tree, sequences=self.sequences)
+        self.assertEqual(str(test), str(self.filtered_tree))
 
 
 if __name__ == "__main__":
