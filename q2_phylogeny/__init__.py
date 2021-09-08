725ed1faed4144ea81950f4c164083c7e3f7b403
Justine Debelius <jwdebelius@users.noreply.github.com>
2021-09-08 20:18:36 +0200

    NEW: method to filter a phylogenetic tree (#82)

--- q2_phylogeny/__init__.py
+++ q2_phylogeny/__init__.py
@@ -10,7 +10,7 @@ from ._util import midpoint_root, robinson_foulds
 from ._fasttree import fasttree
 from ._raxml import raxml, raxml_rapid_bootstrap
 from ._iqtree import iqtree, iqtree_ultrafast_bootstrap
-from ._filter import filter_table
+from ._filter import filter_table, filter_tree
 from ._version import get_versions
 from ._align_to_tree_mafft_fasttree import align_to_tree_mafft_fasttree
 from ._align_to_tree_mafft_iqtree import align_to_tree_mafft_iqtree
@@ -22,4 +22,4 @@ del get_versions
 __all__ = ["midpoint_root", "fasttree", "align_to_tree_mafft_fasttree",
            "raxml", "raxml_rapid_bootstrap", "iqtree", "filter_table",
            "iqtree_ultrafast_bootstrap", "align_to_tree_mafft_iqtree",
-           "align_to_tree_mafft_raxml", "robinson_foulds"]
+           "align_to_tree_mafft_raxml", "robinson_foulds", 'filter_tree']
--- q2_phylogeny/_filter.py
+++ q2_phylogeny/_filter.py
@@ -7,6 +7,8 @@
 # ----------------------------------------------------------------------------
 
 import biom
+import numpy as np
+import qiime2
 import skbio
 
 
@@ -19,3 +21,42 @@ def filter_table(table: biom.Table, tree: skbio.TreeNode) -> biom.Table:
     ids_to_keep = tip_ids & feature_ids
     table.filter(ids_to_keep, axis='observation', inplace=True)
     return table
+
+
+def filter_tree(tree: skbio.TreeNode,
+                table: biom.Table = None,
+                metadata: qiime2.Metadata = None,
+                where: str = None,
+                ) -> skbio.TreeNode:
+    """
+    Prunes a phylogenetic tree to match the input ids
+    """
+    # Checks the input metadata
+    if ((table is None) & (metadata is None)):
+        raise ValueError('A feature table, sequences or metadata must be '
+                         'provided for filtering.')
+    filter_refs = [table, metadata]
+    if np.sum([(ref is not None) for ref in filter_refs]).sum() > 1:
+        raise ValueError('Filtering can only be performed using one reference'
+                         ' file. Please choose between filtering with a '
+                         'feature table, sequences, or metadata.')
+    if (where is not None) & (metadata is None):
+        raise ValueError("Metadata must be provided if 'where' is specified")
+
+    # Gets the list of IDs to keep
+    if table is not None:
+        ids_to_keep = table.ids(axis='observation')
+    if metadata is not None:
+        ids_to_keep = metadata.get_ids(where)
+
+    # Gets the list of tips
+    tip_ids = set([t.name for t in tree.tips()])
+
+    # Checks for an intersection between ids
+    if not set(tip_ids).issuperset(set(ids_to_keep)):
+        raise ValueError('The ids for filtering must be a subset of '
+                         'the tips in the tree.')
+    sub_tree = tree.shear(ids_to_keep)
+    sub_tree.prune()
+
+    return sub_tree
